4fca878c2b1054dffdf44d24b2a560ec5ef35acf
Kishore Kumar S <livekishore2001@gmail.com>
2024-03-07 17:21:08 +0530

    Initial commit

---
+++ app/src/main/java/com/android/recipey/MainActivity.kt
@@ -0,0 +1,109 @@
+package com.android.recipey
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.rememberAsyncImagePainter
+import com.android.recipey.ui.theme.RecipeyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RecipeyTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    RecipeScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun RecipeScreen(modifier: Modifier = Modifier) {
+    val recipeViewModel : MainViewModel = viewModel()
+    val viewState by recipeViewModel.categoriesState
+
+    Box (modifier = Modifier.fillMaxSize()){
+        when {
+            viewState.loading -> {
+                CircularProgressIndicator(modifier.align(Alignment.Center))
+            }
+
+            viewState.error != null -> {
+                Text(text = "Error Occured")
+            }
+
+            else -> {
+                //Display Categories
+                CategoryScreen(categories = viewState.list)
+            }
+        }
+    }
+}
+
+@Composable
+fun CategoryScreen(categories : List<Category>) {
+    LazyVerticalGrid(columns = GridCells.Fixed(2), modifier = Modifier.fillMaxSize()) {
+        items(categories) {
+                category -> CategoryItem(category = category)
+        }
+    }
+}
+
+// How each item looks like
+@Composable
+fun CategoryItem(category: Category) {
+    Column (
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(
+            painter = rememberAsyncImagePainter(category.strCategoryThumb),
+            contentDescription = category.strCategoryDescription,
+            modifier = Modifier
+                .fillMaxSize()
+                .aspectRatio(1f)
+        )
+        Text(
+            text = category.strCategory,
+            style = TextStyle(fontWeight = FontWeight.Bold),
+            modifier = Modifier.padding(top = 4.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RecipeyTheme {
+        RecipeScreen()
+    }
+}
